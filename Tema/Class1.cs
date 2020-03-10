b4cdbd00e247790dd00ca31e04055d1f242aca15
ailen <ailen@DESKTOP-MSJOAMQ>
2020-03-10 15:22:27 +0200

    first commit

---
+++ Tema/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema
+{
+    class Student   
+    {
+        string nume;
+        string prenume;
+        string materie;
+        float nota;
+        string promovat;
+        public Student()
+        {
+            nume = string.Empty;
+            prenume = string.Empty;
+            materie = string.Empty;
+            nota = 0;
+            promovat = string.Empty;
+
+        }
+        public Student(string _nume, string _prenume, string _materie, float _nota)
+        {
+            nume = _nume;
+            prenume = _prenume;
+            materie = _materie;
+            nota = _nota;
+            if (nota >= 5)
+                promovat = "Da";
+            else
+                promovat = "Nu";
+        }
+        public string info()
+        {
+            return string.Format("Nume: {0}\nPrenume: {1}\nMaterie: {2}\nNota: {3}\nPromovat: {4}\n", nume, prenume, materie, nota, promovat);
+        }
+    }
+}
---
+++ Tema/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Student s1 = new Student("Ailenei","Razvan","ED",7);
+			string f1 = s1.info();
+			Console.WriteLine(f1);
+			Student s2 = new Student("Mara","Sabin","ASC",9);
+			string f2 = s2.info();
+			Console.WriteLine(f2);
+
+			Console.ReadKey();
+		}
+	}
+}
